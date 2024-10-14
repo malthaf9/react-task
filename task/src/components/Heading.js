f361e009b29128c7ce3bebd38b8f33a5ae87653d
Altaf Hussain <malthaf989@gmail.com>
2024-10-14 12:27:33 +0530

    Update Heading.js

--- task/src/components/Heading.js
+++ task/src/components/Heading.js
@@ -3,7 +3,6 @@ import DropDowns from "./DropDowns";
 
 const Heading = () => (
   <div className="p-2 m-2 text-xl">
-    {/* Flex layout, with responsive stacking on small screens */}
     <div className="flex flex-col md:flex-row items-center justify-between">
       <h1 className="text-center md:text-left mb-2 md:mb-0">
         Application list (Total 100 people | 1 waiting for approval)
